13cd05cb3a721318bd20d1bf3c9aa3cc68f7675f
mattpurpura <mpurpura212@gmail.com>
2018-09-22 14:54:50 -0700

    challenge 1 completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql  = require("mysql");
+var chalk = require("chalk");
 const log = console.log;
 
 var connection = mysql.createConnection({
@@ -19,13 +20,22 @@ var connection = mysql.createConnection({
 var productsForSale;
 function updateTable(){
     var query = connection.query(
-        "SELECT id, name, price FROM products",
+        "SELECT * FROM products",
         function(err, res){
             productsForSale = res;
-            log(productsForSale);
+            displayItems();
             promptCreateOrder();
         }
     )
+}
+
+function displayItems(){
+    for(let i=0; i< productsForSale.length; i++){
+        log(chalk.yellow(productsForSale[i].name));
+        log(chalk.red("$"+productsForSale[i].price));
+        log(chalk.green("Product ID: " + productsForSale[i].id));
+        log("---------------");
+    }
 }
 
 function promptCreateOrder(){
@@ -41,22 +51,63 @@ function promptCreateOrder(){
             name: "orderQuantity", 
         }
     ]).then(function(response){
-        // placeOrder(response.productID, response.orderQuantity)
+        placeOrder(parseInt(response.productID), parseInt(response.orderQuantity))
     })
 }
 
 function placeOrder(inputId, quantity){
     let productExists = false;
+    let desiredProduct;
     for(let i=0; i < productsForSale.length; i++){
         if (inputId === productsForSale[i].id){
-           return productExists = true
+            desiredProduct = productsForSale[i];
+            productExists = true;
+            break;
         }
     }
     if(productExists === false){
         log("I'm sorry, we do not carry that item.");
+        promptCreateOrder();
     }
     else{
-        
+        if(quantity <= desiredProduct.stock){
+            desiredProduct.stock -= quantity;
+            log("Your order has been placed!");
+            uploadData(desiredProduct.stock, desiredProduct.id);
+        }
+        else{
+            log("Insufficient Quantity");
+            promptCreateOrder();
+        }
     }
+}
+
+function uploadData(stock, id){
+    var query = connection.query(
+        "UPDATE products SET stock = ? WHERE id = ?", [stock, id],
+        function(err, res){
+            log("Updated"); 
+            promptNewOrder();
+        }
+    )
+}
+
+function promptNewOrder(){
+    inquirer.prompt([
+        {
+            type: "confirm", 
+            message: "Would you like to place another order?",
+            name: "newOrder"
+        }
+    ]).then(function(response){
+        if(response.newOrder === true){
+            displayItems();
+            promptCreateOrder();
+        }
+        else{
+            log("Goodbye");
+            connection.end();
+        }
+    })
 }
 
